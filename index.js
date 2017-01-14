8fadf61971c33ba20a6d312063f1000e8a452440
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-01-13 23:01:27 -0800

    server is working

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ import startServer from './src/server';
 
 export const store = makeStore();
 startServer(store);  //starts server
+
+store.dispatch({
+  type: "SET_ENTRIES",
+  entries: require('./entries.json')
+});
+store.dispatch({type: "NEXT"});
